54155e685a5cef0ea083a30d7e52da1100697d28
Florian Reimold <11774314+FlorianReimold@users.noreply.github.com>
2024-03-15 13:27:15 +0100

    trying to fix integration test

--- thirdparty/build-udpcap.cmake
+++ thirdparty/build-udpcap.cmake
@@ -16,7 +16,7 @@ set(UDPCAP_THIRDPARTY_ENABLED          ON)
 set(UDPCAP_THIRDPARTY_USE_BUILTIN_ASIO OFF)
 
 # Add udpcap library from subdirectory
-add_subdirectory("${CMAKE_CURRENT_LIST_DIR}/udpcap" EXCLUDE_FROM_ALL)
+add_subdirectory("${CMAKE_CURRENT_LIST_DIR}/udpcap" thirdparty/udpcap EXCLUDE_FROM_ALL)
 add_library(udpcap::udpcap ALIAS udpcap)
 
 # Reset static / shared libs to old value
